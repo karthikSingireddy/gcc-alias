4c035b09e1f6b462df695a0d9f0f3d876c8135c2
karthikSingireddy <skarthikreddy189@gmail.com>
2020-07-22 11:57:00 -0700

    added gccAlias.sh

---
+++ gccAlias.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo $#
+
+if [ $# -lt 5 ] || [ $# -gt 5 ]; then
+  echo "./gccAlias <compileCommand> <runCommand> <compileAndRunCommand> <c file> <output file>"
+  exit 1
+fi
+
+compileCommand=$1
+runCommand=$2
+compileAndRunCommand=$3
+cFile=$4
+outputFile=$5
+
+alias ${compileCommand}="gcc -Wall -g -std=c11 -o ${outputFile} ${cFile}"
+echo "alias ${compileCommand}=\"gcc -Wall -g -std=c11 -o ${outputFile} ${cFile}\""
+
+alias ${runCommand}="./${outputFile}"
+echo "alias ${runCommand}=\"./${outputFile}\""
+
+alias ${compileAndRunCommand}="${compileCommand} && ${runCommand}"
+echo "alias ${compileAndRunCommand}=\"${compileCommand} && ${runCommand}\""
